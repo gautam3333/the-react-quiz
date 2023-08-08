fb2dfabe1a80f7ad5b3df5a60e5800f5be01db44
Gautam Sarkar <gautam.sarkar3333@gmail.com>
2023-08-08 10:52:23 +0530

    add:finishing a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
+import FinishScreen from './FinishScreen';
 
 const initialState = {
   questions: [],
@@ -16,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 const reducer = function (state, action) {
@@ -43,6 +45,14 @@ const reducer = function (state, action) {
 
     case 'nextQuestion':
       return { ...state, index: state.index + 1, answer: null };
+
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
 
     default:
       throw new Error('Unkonwn action');
@@ -87,8 +97,16 @@ const App = function () {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === 'finished' && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
         )}
       </Main>
     </div>
--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,12 +1,26 @@
-const NextButton = function ({ dispatch, answer }) {
+const NextButton = function ({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: 'nextQuestion' })}
-    >
-      Next
-    </button>
-  );
+
+  if (index < numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'nextQuestion' })}
+      >
+        Next
+      </button>
+    );
+  }
+
+  if (index === numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'finish' })}
+      >
+        Finish
+      </button>
+    );
+  }
 };
 export default NextButton;
